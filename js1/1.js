fc8ad35317859ddb23ba57b4e5e067c06f11e34e
Zhen Yang <webyangfullstack@gmail.com>
2020-05-04 01:05:26 +1000

    until Array part

--- js1/1.js
+++ js1/1.js
@@ -1,1 +1,47 @@
-var a = 1
+console.log("hello world")
+console.log(2+2)
+console.log(10%3)
+console.log(2**4)
+console.log(0.1+0.2)
+var a =2
+console.log(a+1)
+console.log(a++)
+var b = true
+var c = {
+    name:'yang',
+    age:'26',
+    school:"uts"
+}
+console.log(c.school)
+//严格模式，没生命的变量都会报错
+'use strict'
+console.log(aa)
+var aa = 10
+console.log(1===1)
+
+console.log(4+3+"3")
+console.log("4"+3+3)
+console.log("abc"*2)
+//function
+let sum = function(a,b){
+    return a+b
+}
+var re = sum(9,8)
+console.log(re)
+
+var number = 111222
+var no = number.toString()
+var no16 = number.toString(16)
+console.log(no)
+console.log(no16)
+
+//array
+var arr = ['a','b','c']
+console.log(arr.length)
+console.table(arr)
+
+
+
+
+
+
