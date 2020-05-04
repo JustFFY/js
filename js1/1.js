e6df56b14554d316a488e02141568ed603335b9d
Zhen Yang <webyangfullstack@gmail.com>
2020-05-04 15:50:26 +1000

    until dom js

--- js1/1.js
+++ js1/1.js
@@ -40,8 +40,85 @@ var arr = ['a','b','c']
 console.log(arr.length)
 console.table(arr)
 
+var a1 = [1,2,3].map(x=>x*2)
+console.log(a1)
+console.log(a1.filter(x=>x>5))
 
+//condition
+var cd = 10
+if(cd === 10){
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
 
+var age = 10
+var description
+if (age <18){
+    description = "young"
+}
+else{
+    description = "old"
+}
+console.log(description)
 
+var description = age < 18 ? "young" : "old" 
+console.log(description)
+
+//loop 
+for(var i =0; i<10;i++){
+    console.log(i)
+}
+console.log(i)//let i 会报错
+
+//continue break 
+//乘法口诀
+    for(let i = 1;i<=9;i++){
+        let row = ""
+        for(let j = i; j<=9;j++){
+            row+=(i+"*"+j+"="+i*j)+"\t"
+        }
+        console.log(row)
+    }
+
+//
+var a = 100
+function test(){
+    var aaaa = 300
+    function test2(){
+        var aaaa =200
+        console.log(aaaa)
+    }
+    test2()
+    console.log(aaaa)
+}
+test()
+//console.log(aaaa)
+
+//object
+var obj={}
+var yang = {
+    name:"zhen",
+    age:26,
+    school:"uts"
+}
+console.log(yang.school)
+console.log(yang["name"])
+
+//
+const nums = [2,7,11,15]
+const target = 9
+var twoSum = function (nums, target){
+    let map={}
+    for(let i =0;i<nums.length;i++){
+        const diff = target - nums[i];
+        if(map[diff]!==undefined){
+            return[i,map[diff]];
+        }
+        map[nums[i]] = i;
+    }
+}
+console.log(twoSum(nums,target))
 
 
